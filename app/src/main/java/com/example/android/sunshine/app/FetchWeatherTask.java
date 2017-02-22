d697ed51b5d417fb46dcd11554b5259dec6d17bc
lmadara1 <leonard.madarang@gmail.com>
2017-02-21 19:30:08 -0800

    fix FetchWeatherTask.java and merge addLocation

--- app/src/main/java/com/example/android/sunshine/app/FetchWeatherTask.java
+++ app/src/main/java/com/example/android/sunshine/app/FetchWeatherTask.java
@@ -116,7 +116,7 @@ public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
                 WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING + " = ?",
                 new String[]{locationSetting},
                 null
-                );
+        );
         // If it exists, return the current ID
         if(cursor.moveToFirst()){
             return cursor.getLong(cursor.getColumnIndex("_id"));
@@ -356,12 +356,14 @@ public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
             final String FORMAT_PARAM = "mode";
             final String UNITS_PARAM = "units";
             final String DAYS_PARAM = "cnt";
+            final String APPID_PARAM = "APPID";
 
             Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
                     .appendQueryParameter(QUERY_PARAM, params[0])
                     .appendQueryParameter(FORMAT_PARAM, format)
                     .appendQueryParameter(UNITS_PARAM, units)
                     .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                    .appendQueryParameter(APPID_PARAM, BuildConfig.OPEN_WEATHER_MAP_API_KEY)
                     .build();
 
             URL url = new URL(builtUri.toString());
